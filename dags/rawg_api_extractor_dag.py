efe250a55a4ade916c4fc3e28ecddbc0a0c8bb7a
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-12-10 14:24:27 +0530

    dag code to fetch data from rawg api

--- dags/rawg_api_extractor_dag.py
+++ dags/rawg_api_extractor_dag.py
@@ -6,9 +6,10 @@ from airflow.providers.google.cloud.hooks.gcs import GCSHook
 from airflow.providers.google.cloud.hooks.secret_manager import SecretsManagerHook
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.utils.dates import days_ago
+from airflow.models import Variable
 
 # Custom modules import
-
+from utils.rawg_api_caller import RAWGAPIResultFetcher
 
 
 
@@ -32,36 +33,52 @@ default_args = {
 )
 def rawg_api_extractor_dag():
 
-    # Using SecretsManagerHook we make a call the GCP Secrets Manager and get the corresponding API Key
+    # Fetch variable values for API Key and Page Number
+    rawg_api_key = Variable.get('rawg_api_key')
+    rawg_page_number = int(Variable.get('api_page_number', default_var=1))
+
+    # Task to get Game IDs to fetch data from in subsequent task
     @task
-    def get_rawg_api_key() -> str:
+    def get_rawg_api_game_ids(api_key: str, page_number: int) -> []:
         """
-            Fetches API Key from Secrets Manager.
+            Fetches List of Game IDs to be later used to make get call to /games/{id} endpoint.
 
             Args:
-                gcp_conn_id: Service Account Connection added to Airflow which has read access to Secrets Manager.
-                secret_id: Secret Name with which it has been named at Secrets Manager end.
+                api_key: Needed to make calls to RAWG API , required for every call per documentation.
+                page_number: Required to paginate API calls , will increment with every run 
 
             Returns:
-                API Key as a string value.
+                Game IDs as a list value
         """
-        secrets_manager_hook = SecretsManagerHook(gcp_conn_id='gcp')
-        api_key = secrets_manager_hook.get_secret(secret_id='RAWG_API_KEY')
-        
-        return api_key
+        rawg_http_client_games_list = RAWGAPIResultFetcher()     
+        return rawg_http_client_games_list.get_unique_ids_per_endpoint(api_key, page_number)
 
-    # TODO - Write tasks that calls different endpoints of RAWG API using the API Key and upload results to GCS
+    # Task to use the game ids to iterate upon and create the 5 flattened tables to be uploaded to GCS and then loaded to Bigquery later
     @task
-    def generate_base_url(api_key: str) -> list:
-        base_url = 'https://api.rawg.io/api'
-        endpoint = 'games'
-        
-        return generate_full_url(base_url, endpoint, api_key)
+    def get_game_id_related_data(api_key: str, game_ids_list: list, page_number: int) -> None:
+        """
+            Fetches individual entries for Game ID which will be flattened to convert to 5 csv tables and save to local file path.
 
-    
+            Args:
+                api_key: Needed to make calls to RAWG API , required for every call per documentation.
+                game_ids_list: List of Game IDs returned from previous task
+                page_number: Required to paginate API calls , will increment with every run 
 
-    api_key = get_rawg_api_key()
-    get_url = generate_base_url(api_key)
+            Returns:
+                None
+        """
+        rawg_http_client_game_detail_fetcher = RAWGAPIResultFetcher()
+        games_df, ratings_df, platforms_df, genre_df, publisher_df = rawg_http_client_game_detail_fetcher.get_game_details_per_id(api_key, game_ids_list, page_number)
+
+        # Save the files as CSV directly to GCS , post creating GCS bucket variable
+        # Update page number to fetch from the consecutive one in the next run
+        next_page_number = int(rawg_page_number) + 1
+        Variable.set("api_page_number", next_page_number)
+
+
+    game_ids_list = get_rawg_api_game_ids(rawg_api_key, rawg_page_number)
+    game_details_extractor = get_game_id_related_data(rawg_api_key, game_ids_list, rawg_page_number)
+
 
 rawg_api_extractor_dag()
 
--- dags/utils/rawg_api_caller.py
+++ dags/utils/rawg_api_caller.py
@@ -1,51 +1,105 @@
 import requests
+import pandas as pd
 
 # Logger factory class import
 from utils.logger import LoggerFactory
 
 # Custom modules import
-from utils.gcp_utils import CloudUtils
 from utils.url_generator import generate_full_url
 
+# Common values
 base_url = 'https://api.rawg.io/api'
+file_save_path = '/usr/local/airflow/include/raw_extracted_data'
+endpoint = 'games'
+
+info_logger = LoggerFactory.get_logger('INFO')
+error_logger = LoggerFactory.get_logger('ERROR')
+
 
 class RAWGAPIResultFetcher():
   """
-    A class that makes GET calls to the RAWG API using the API Key fetched from secrets manager.
-
-    Attributes:
-        api_key: A string representing the API Key for your RAWG Developer Account.
+    A class that makes GET calls to the RAWG API using the API Key fetched from Airflow Variable.
 
     Methods:
-        get_games() -> {}:
-          Retrieves list of games matching the passed query.
+        get_unique_ids_per_endpoint() -> []:
+          Retrieves list of game IDs matching the passed query.
         Returns:
-          JSON Response for the endpoint - https://api.rawg.io/docs/#operation/games_list
+          List containing the Game IDs
+
+        get_game_details_per_id() -> None:
+          Fetches game details for the fetched ID list and converts to CSV
+        Returns:
+          N/A    
   """
+
+  # def create_session_object():
+  #   """
+  #     Generates sessions to allow usage of connection pooling
+
+  #     This method creates a session object that we can use to keep a TCP connection alive for a specific time.
+
+  #     Returns:
+  #         Session object that can be later used to make requests.
+  #   """
+  #   rawg_api_session = requests.Session()
+  #   info_logger.info(f'Generated session object: {rawg_api_session} for the current iteration.')
+
+  #   return rawg_api_session
   
-  def get_tags(api_key: str) -> {}:
-    pass
+  def get_unique_ids_per_endpoint(self, api_key: str, page_number: int) -> []:
+    """
+      Gets unique IDs related to the respective endpoint
 
-  def get_platforms(api_key: str) -> {}:
-    pass
+      This method sends a GET request to the RAWG API to retrieve
+      list of results that are matching the search query.
 
-  def get_genres(api_key: str) -> {}:
-    pass
+      Reference endpoint - https://api.rawg.io/docs/
 
-  def get_developers(api_key: str) -> {}:
-    pass
+      Args:
+          api_key: API Key needed to make calls to RAWG API endpoint.
+          endpoint: Endpoint name to be used to get respective data.
+          page_number: Page Number for the specific call , to be fetched from Airflow Variable for every run in incremented fashion.
+          List of Endpoints: games, creators, tags, developers, platforms, genres etc
 
-  def get_creators(api_key: str) -> {}:
-    pass
+      Returns:
+          IDs in the form of list using the responses mentioned in the endpoint URL.
+    """
     
-  def get_games(api_key: str) -> {}:
+    info_logger.info(f'Using page number: {page_number} for the following endpoint: {endpoint} call session to get IDs.')
+
+    # List of endpoints to iterate and get IDs from
+    # endpoints_list = ['games', 'tags', 'genres', 'developers', 'creators']
+
+    # Empty lists to be later appended while iterating over endpoints
+    endpoint_ids_list = []
+
+    # Generate base URL to make a GET call to the respective endpoint
+    generate_api_url = generate_full_url(base_url,endpoint,api_key)
+
+    endpoint_params = {
+      'page_size': '10000',
+      'page': page_number,
+      'platforms_count': '6'
+    }
+
+    endpoint_response = requests.get(generate_api_url, params=endpoint_params)
+
+    # If there is response then only do further processing which involves flattening the data from results list and fetching only IDs
+    if endpoint_response.status_code == 200:
+      endpoint_response_json = endpoint_response.json()
+      flattened_results_df = pd.json_normalize(endpoint_response_json,record_path=['results'])
+      endpoint_ids_list = flattened_results_df['id'].tolist()
+      info_logger.info(f'Fetched {len(endpoint_ids_list)} unique IDs for {endpoint} endpoint')
+
+    return endpoint_ids_list
+  
+  def get_game_details_per_id(self, api_key: str, endpoint_ids: list, page_number: int) -> pd.DataFrame:
     """
       Gets games related to the entered search query
 
-      This method sends a GET request to the RAWG API to retrieve
-      list of games that are matching the search query.
+      This method sends a GET request to the RAWG API to retrieve details of the game based on the ID used.
 
-      Reference endpoint - https://api.rawg.io/docs/#operation/games_list
+      Reference endpoint - https://api.rawg.io/docs/#operation/games_read
 
       Args:
           self: An instance of the class containing the RAWG API Key.
@@ -58,14 +112,61 @@ class RAWGAPIResultFetcher():
           with the status code and error message returned by the API.
     """
     endpoint = 'games'
-    game_request_url = generate_full_url(base_url, endpoint, api_key)
     payload = {}
     headers = {}
 
-    # Run a GET call to the games endpoint
-    response = requests.request("GET", game_request_url, headers=headers, data=payload).json()
+    # Keys to include when flattening /games/id values to create Game Dataframe
+    keys_game_ids_df_response = [
+      "id", # ID of the said Game
+      "slug", # Slug formatted name of the Game eg - Witcher 3 slug version is : witcher-3
+      "name_original", # original name of the game , would come handy in visualizing data
+      "description_raw", # Description or short summary of the game
+      "metacritic", # Metacritic rating of the game 
+      "released", # Date when the game was released
+      "tba", # Is it an upcoming game or has it been released?
+      "updated", # Date when the info regarding the game was last updated
+      "rating", # User ratings for the game , its out of 5
+      "rating_top", # Rounded off version of rating , for eg 4.01 becomes 4 , 4.99 becomes 5
+      "playtime" # Time taken to complete the given game
+      ]
 
-    if response.status_code != 200:
-      raise Exception('Error:', response.status_code, response.text)
-    
-    LoggerFactory.get_logger('./logs/info.log','INFO').info(f'Fetched {len(response["results"])} games for the current session')
+    games_df = pd.DataFrame()
+    ratings_df = pd.DataFrame()
+    platforms_df = pd.DataFrame()
+    genre_df  = pd.DataFrame()
+    publisher_df = pd.DataFrame()
+
+    # Run a GET call to the games endpoint
+    for game_id in endpoint_ids:
+      response = requests.request("GET", f"{base_url}/{endpoint}/{game_id}?key={api_key}", headers=headers, data=payload)
+      info_logger.info(f'Fetched game related data for {game_id} for {endpoint} endpoint')
+
+      if response.status_code == 200:
+        # Convert to JSON and flatten the response
+        games_json = response.json()
+
+        # Processing steps to create Dataframe containing only game related data
+        games_df_raw = pd.json_normalize(games_json)
+        games_df_filtered = games_df_raw[keys_game_ids_df_response]
+        games_df = pd.concat([games_df, games_df_filtered], ignore_index=True)
+
+        # Processing steps to create Dataframe containing only Ratings related Data
+        # Use the ID key to use as Foreign Key while loading into DB
+        ratings_df_flattened = pd.json_normalize(games_json, record_path=['ratings'], meta=['id'], meta_prefix='ratings_game_')
+        ratings_df = pd.concat([ratings_df, ratings_df_flattened], ignore_index=True)
+
+        # Preprocessing steps to create Dataframe containing platforms data
+        platforms_df_flattened = pd.json_normalize(games_json, record_path=['platforms'], meta=['id'], meta_prefix='platforms_game_')
+        platforms_df = pd.concat([platforms_df, platforms_df_flattened], ignore_index=True)
+
+        # Preprocessing steps to create Dataframe containing Genre data
+        genre_df_flattened = pd.json_normalize(games_json, record_path=['genres'], meta=['id'], meta_prefix='genre_game_')
+        genre_df = pd.concat([genre_df, genre_df_flattened], ignore_index=True)
+        
+        # Preprocessing steps to create Dataframe containing Publisher data
+        publisher_df_flattened = pd.json_normalize(games_json, record_path=['publishers'], meta=['id'], meta_prefix='publisher_game_')
+        publisher_df = pd.concat([publisher_df, publisher_df_flattened], ignore_index=True)
+
+    # Log the dimensions of each flattened file for tracking
+    info_logger.info(f"Dimension of the data fetched and flattened for the following #{page_number} iteration: Games Table {games_df.shape}, Ratings Table {ratings_df.shape}, Platforms Table {platforms_df.shape}, Genre Table {genre_df.shape}, Publisher Table {publisher_df.shape}")
+    return games_df, ratings_df, platforms_df, genre_df, publisher_df
