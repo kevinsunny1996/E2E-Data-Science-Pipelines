210b3cb771d0c1cf24c73aac4d1bdeed5e91087b
Kevin Parasseril <kevinsunny1996@gmail.com>
2024-07-09 23:13:36 +0530

    Fixed rating category games count logic

--- dags/dbt/games_analyzer_rawg_api/models/dim/dim_ratings.sql
+++ dags/dbt/games_analyzer_rawg_api/models/dim/dim_ratings.sql
@@ -8,8 +8,9 @@
 SELECT
     id AS rating_id,
     title AS rating_category,
-    COUNT(id) AS rating_count
-FROM {{ source('rawg_api_raw', 'ratings') }}
-WHERE game_id IN (SELECT game_id FROM {{ ref('stg_games') }} WHERE metacritic != 'None')
+    COUNT(ratings_raw.game_id) AS rating_count
+FROM {{ source('rawg_api_raw', 'ratings') }} ratings_raw LEFT JOIN {{ ref('stg_games') }} games_eph
+ON ratings_raw.game_id=games_eph.game_id AND ratings_raw.id=games_eph.rating_top
+WHERE games_eph.metacritic != 'None'
 GROUP BY 1, 2
 ORDER BY 1
